b3dd3d8624c78a75134d9b9a629bd17c2d961272
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-07-25 10:24:34 -0300

    Js manhã

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,16 @@
+function hello(){
+    alert("Hello World")
+}
+
+let nome = document.getElementById('nome');
+let email = document.getElementById('email');
+let assunto = document.getElementById('assunto');
+
+function enviar(){
+    if(nome.value == "" || email.value == "" || assunto.value == ""){
+        alert("Todos os campos devem ser preenchidos")
+    }else{
+        alert("Seu feedback foi enviado")
+    }
+}
+
